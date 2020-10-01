44b94677d46a9fb0a6b5ee83c62c8f06a8922b4b
Dean Power <D19124840@mytudublin.ie>
2020-10-01 18:17:32 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Social Network Analysis module group project 2020',
+    author='Dean Power',
+    license='',
+)
